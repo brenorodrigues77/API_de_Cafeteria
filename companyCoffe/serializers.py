d5c6765524979304abd68157111a42c3b3167421
brenorodrigues77 <breno7rodrigues@gmail.com>
2025-01-20 11:06:09 -0300

    calculando avaliações das cafeteria
    Nesse commit foi feito a logica de calculo das avaliações, data de criação e limite de caracteres de comentários das empresas, através do serializers de "companyCoffe". Sendo possível saber quantas estrelas tem cada um, limite de texto por comentário e cadastro de empresas a partir do ano de 1950.

--- companyCoffe/serializers.py
+++ companyCoffe/serializers.py
@@ -1,7 +1,26 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from companyCoffe.models import companyCoffe
 
 class companyCoffeSerializers(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = companyCoffe
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = round(obj.review.aggregate(Avg('stars'))['stars__avg'], 1)
+        if rate:
+            return rate
+        return None
+
+    def validate_release_date(self, value):
+        if value.year < 1950:
+          raise serializers.ValidationError('A data de criação nao pode ser menor que 1950.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('O campo resumo nao pode ter mais que 500 caracteres.')
+        return value
